6ba685389deb57141cd6b6b9c837d9b0e726c21f
Shivam <mittalshivam440@gmail.com>
2024-10-01 00:44:32 +0530

    check auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,44 +1,51 @@
 import jwt, { decode } from 'jsonwebtoken'
 import Doctor from '../models/DoctorSchema.js';
 import User from '../models/UserSchema.js';
-import {DOCTOR_TOKEN} from '../constant/config.js'
+import { DOCTOR_TOKEN } from '../constant/config.js'
 import { ErrorHandler } from '../utils/utility.js';
 import { TryCatch } from './error.js';
 
-export const isAuthentication = TryCatch((req, res, next) => {
-   
+export const isAuthentication = TryCatch(async (req, res, next) => {
+
     const token = req.cookies[DOCTOR_TOKEN]
 
     if (!token) {
         return next(new ErrorHandler("Please login to access this route", 401))
     }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
 
-        req.userId = decoded.id
-        req.role = decoded.role
+    req.userId = decoded.id
+    req.role = decoded.role
 
-        console.log("Authenticated userId:", req.userId);
-        console.log("Authenticated role:", req.role);
+    console.log("Authenticated userId:", req.userId);
+    console.log("Authenticated role:", req.role);
 
-        next();
-  
+    const user = await User.findById(req.userId) || await Doctor.findById(req.userId)
+
+    if(!user){
+        return next(new ErrorHandler("User not found", 404))
+    }
+
+    req.user = user
+    next();
+
 })
 
 export const restricte = roles => TryCatch(async (req, res, next) => {
     const userId = req.userId;
     console.log(userId)
 
-        const user = await User.findById(userId) || await Doctor.findById(userId);
+    const user = await User.findById(userId) || await Doctor.findById(userId);
 
-        if (!user) {
-            return next(new ErrorHandler("User not found", 404))
-        }
+    if (!user) {
+        return next(new ErrorHandler("User not found", 404))
+    }
 
-        if (!roles.includes(user.role)) {
-            return next(new ErrorHandler("You're not authorized", 401))
-        }
+    if (!roles.includes(user.role)) {
+        return next(new ErrorHandler("You're not authorized", 401))
+    }
 
-        next();
-   
+    next();
+
 })
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,19 @@
 import express from 'express'
 import { register, login, logout } from '../controllers/authController.js'
 import { loginValidator, registerValidator, validateHandler } from '../lib/validator.js'
-
+import {isAuthentication} from '../middleware/auth.js' 
 const router = express.Router()
 
 router.post('/register', registerValidator(), validateHandler, register)
 router.post('/login', loginValidator(), validateHandler, login)
 router.post('/logout', logout)
-
+router.get('/checkauth', isAuthentication, (req,res) => {
+    const user = req.user
+    console.log("user", user)
+    res.status(200).json({
+        success : true,
+        message : "User Authenticated !!",
+        user
+    })
+})
 export default router
